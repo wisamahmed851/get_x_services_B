46a93501b6b4e974503b19d7fb4bb30f7365171e
toobaashar13 <165471603+toobaashar13@users.noreply.github.com>
2025-06-30 20:03:11 +0500

    tooba
    UPDATE OF VEHICLE REGISTRATION

--- src/vehicle-registration/dtos/vehicle-registration.dto.ts
+++ src/vehicle-registration/dtos/vehicle-registration.dto.ts
@@ -1,8 +1,42 @@
+import { IsNotEmpty, IsOptional } from "class-validator";
+
 export class CreateVehicleRegistrationDto {
-  vehicleName: string;
-  vehiclemodel: string;
-  registrationNumber: string;
-  color: string;
-  company: string;
-  image: string;
+    @IsNotEmpty()
+    vehicleName: string;
+
+    @IsNotEmpty()
+    vehiclemodel: string;
+
+    @IsNotEmpty()
+    registrationNumber: string;
+
+    @IsNotEmpty()
+    color: string;
+
+    @IsNotEmpty()
+    company: string;
+
+    @IsNotEmpty()
+    image: string;
 }
+
+export class UpdateVehicleRegistrationDto {
+    @IsOptional()
+    vehicleName: string;
+
+    @IsOptional()
+    vehiclemodel: string;
+
+    @IsOptional()
+    registrationNumber: string;
+
+    @IsOptional()
+    color: string;
+
+    @IsOptional()
+    company: string;
+
+    @IsOptional()
+    image: string;
+}
+
